e61e8ab406851e19841e952144494b6adf2981c4
inaijin <kourosh@Kouroshs-MBP.lan>
2024-08-18 14:52:49 +0330

    Phase1-Color-Detection

---
+++ Phase1_Image_Processing/Projects/Color-Detection/util.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+# HSV Is A Circular Shape So Special Handling Is Needed Near Wrap Arounds
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
